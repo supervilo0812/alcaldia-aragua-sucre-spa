2939bc9147b1328b419c54927f2719ece7e99636
root <root@galactica.galactica>
2020-12-06 20:01:56 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <router-view name="header" />
+      <div>
+        <v-overlay :value="overlay">
+          <v-progress-circular
+            indeterminate
+            size="64"
+          />
+        </v-overlay>
+        <router-view />
+      </div>
+    <router-view name="footer" />
+  </v-app>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  export default {
+    name: 'App',
+    data () {
+      return{
+      }
+    },
+      computed: {
+      ...mapGetters({
+        overlay: 'overlay',
+      }),
+    },
+  }
+</script>
